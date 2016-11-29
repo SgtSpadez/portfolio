a7d2afaf3784930959e71913d02a073b389b93ed
Eddie <edward.sartori91@gmail.com>
2016-11-29 18:41:40 +1100

    Added scroll effect to header and anchored sections:

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
 // jQuery
+$(document).ready(function() {
+
+  var $root = $('html, body');
+  $('a').click(function() {
+      $root.animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+      return false;
+  });
+
+  $(function(){
+    var lastScrollTop = 0, delta = 5;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+
+       if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+       if (st > lastScrollTop){
+           // downscroll
+           $("#header").addClass('hide-header')
+       } else {
+          // upscroll
+          $("#header").removeClass('hide-header');
+       }
+       lastScrollTop = st;
+    });
+});
+
+});
